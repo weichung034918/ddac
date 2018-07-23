5e2b31f56aedcf6367a0e3138783f1dffe225f8f
weichung034918 <weichung034918@gmail.com>
2018-07-23 23:40:09 +0800

    webjob to update status from approved to pending arrival

---
+++ ddac/webjob/updateApprovedItem.php
@@ -0,0 +1,25 @@
+﻿<?php
+	try
+	{
+		$connection = new PDO("sqlsrv:Server=tp034918.database.windows.net;Database=ddacassignment", "tp034918", "zaq1@WSX");
+		$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$connection->setAttribute(PDO::SQLSRV_ATTR_ENCODING, PDO::SQLSRV_ENCODING_SYSTEM);
+	}catch (Exception $e)
+	{
+		echo $e->getMessage();
+		die('Connection could not be established.<br />');
+	}
+	try
+	{
+		$sql = "UPDATE shipping SET status = 'PENDING ARRIVAL' WHERE status = 'APPROVED';";
+		$query = $connection->prepare($sql);
+		$query->execute();
+		$result = $query->fetchAll(PDO::FETCH_ASSOC);
+	}catch (Exception $e)
+	{
+		die('Cant fetch rows.');
+	}
+	foreach ($result as $r)
+	{
+	}
+?>
